6bafed7799817270f57193b90cb10ebcbdd97b72
Princess <syhcovks@gmail.com>
2023-01-16 22:01:09 +0500

    add divider to list chat

--- app/src/main/java/com/example/firebase_chat/UserListActivity.java
+++ app/src/main/java/com/example/firebase_chat/UserListActivity.java
@@ -3,6 +3,7 @@ package com.example.firebase_chat;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -95,6 +96,9 @@ public class UserListActivity extends AppCompatActivity {
     private void buildRecyclerView() {
         userRecyclerView = findViewById(R.id.userListRecyclerView);
         userRecyclerView.setHasFixedSize(true);
+        userRecyclerView.addItemDecoration(new DividerItemDecoration(
+                userRecyclerView.getContext(), DividerItemDecoration.VERTICAL
+        ));
         userLayoutManager = new LinearLayoutManager(this);
         userAdapter = new UserAdapter(userArrayList);
 
